7c16f18ae787c5ce5ae6192af70ca52249bef3b3
Stephen Niedzielski <stephen@niedzielski.com>
2012-12-08 17:36:46 -0700

    Add readme.

---
+++ README.mk
@@ -0,0 +1,1 @@
+The Dreamcatcher kernel is a thin, preemptive kernel for MCF52233 and PIC18F4550 chips, with the latter also supporting a simple USB driver. This was a humble student project I wrote from scratch one semester sophomore year.
